2ff7e46c36837d2d3d5bd1d0677ae3535d62b30a
mvrthn <martin@agb.home.pl>
2025-02-24 16:58:19 +0100

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 #include "game.hpp"
 
@@ -7,8 +8,25 @@ int main() {
     Noodle::Buffer buffer;  
     Noodle::Game<3> game(buffer);
     int x;
+    std::unordered_map<int, std::string> map = {
+
+        {128, "POOL"},
+        {129, "PLAYER"},
+        {130, "HAND"},
+        {131, "BOWL"},
+        {132, "ADD"},
+        {133, "DEL"},
+        {134, "CLR"},
+        {135, "OFF"}
+    };
     while(buffer >> x) {
-        std::cout << x << '\n';
+        if(map.contains(x)) {
+            std::cout << map[x];
+        }
+        else {
+            std::cout << x;
+        }
+        std::cout << '\n';
     } 
     return 0;
 }
